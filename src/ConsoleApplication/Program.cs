56560320be3bdc645ba59c21eaa6f35ed4a3cabe
mery <mcpalanques@gmail.com>
2015-09-09 12:14:58 +0200

    download document

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -13,11 +13,11 @@ namespace ConsoleApplication
 		{
 			// These are the authorisation properties of your app.
 			// You can find the values in the App Center when you are maintaining the app.
-			const string clientId = "00000000-0000-0000-0000-000000000000";
-			const string clientSecret = "...";
+			const string clientId = "448fa0f2-988f-48e7-a6db-1bb075036d73";
+			const string clientSecret = "yz45UT0HVRUz";
 
 			// This can be any url as long as it is identical to the callback url you specified for your app in the App Center.
-			var callbackUrl = new Uri("http://www.exact.com"); 
+			var callbackUrl = new Uri("https://github.com/artzain/gorkasapp.git");
 
 			var connector = new Connector(clientId, clientSecret, callbackUrl);
 			var client = new ExactOnlineClient(connector.EndPoint, connector.GetAccessToken);
@@ -28,5 +28,17 @@ namespace ConsoleApplication
 
 			Debug.WriteLine("Account {0} - {1}", account.Code.TrimStart(), account.Name);
 		}
+
+		private void DownloadFile()
+        {
+            OAuthUtility.GetAsync("https://api.dropboxapi.com/1/metadata/auto/",
+                new HttpParameterCollection {
+                    {"path", "https://www.dropbox.com/s/3m8738gnkikx14w/exactonline-api-dotnet-client.zip?dl=0" },
+                    {"list", "true" },
+                    {"access_token",Properties.Settings.Default.AccessToken }
+                } ,
+            callback: Result);
+        }
+
 	}
 }
